531dc2678f34df4fcca9cb60e7e4b42b9a84d1fe
Surnekk <surnekk@iMac-15s-iMac.local>
2022-09-23 11:27:24 +0800

    angi deer

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -1,62 +1,68 @@
 import * as React from "react";
-import { Box, TextField , Container, Typography, Button} from "@mui/material";
+import {
+  Box,
+  TextField,
+  Container,
+  Typography,
+  Button,
+  Grid,
+} from "@mui/material";
 import { Konoha } from "../images/Konoha";
 import { KonohaUp } from "../images/KonohaUp";
-import {sendSignInLinkToEmail} from "firebase/auth"
+import { sendSignInLinkToEmail } from "firebase/auth";
 import { auth } from "../FirebaseForThisProject/Firebase";
 
 export const Login = () => {
-    const [email,setEmail]=React.useState()
-    const actionCodeSettings = {
-        url: 'https://foody-5ec4a.web.app/',
-        handleCodeInApp: true,
-    };
-    const sentMessage=()=>{
-      console.log(email);
-      sendSignInLinkToEmail(auth, email, actionCodeSettings)
+  const [email, setEmail] = React.useState();
+  const actionCodeSettings = {
+    url: "https://foody-5ec4a.web.app/",
+    handleCodeInApp: true,
+  };
+  const sentMessage = () => {
+    console.log(email);
+    sendSignInLinkToEmail(auth, email, actionCodeSettings)
       .then(() => {
-          console.log('amjilttai')
+        console.log("amjilttai");
         window.localStorage.setItem("emailForSignIn", email);
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        console.log(errorCode,errorMessage)
-
+        console.log(errorCode, errorMessage);
       });
-    }
-    return (
-        <Container
-            display="flex"
-            justifyContent="center"
-        >  
-          <Box 
-            display="flex"
-            justifyContent="space-between"
+  };
+  return (
+    <Grid
+      display="flex"
+      justifyContent="center"
+      flexDirection="column"
+      alignItems="center"
+    >
+      <Typography color="#474B5A" variant="h4">
+        Нэвтрэх
+      </Typography>
+      <Box
+        display="flex"
+        flexDirection="column"
+        gap="15px"
+        width="400px"
+        mt="30px"
+      >
+        <TextField
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          label="ИМэйл"
+          fullWidth
+        />
+        <TextField label="Нууц үг" fullWidth />
+        <Button
+          onClick={() => sentMessage()}
+          variant="contained"
+          sx={{ color: "#fff" }}
         >
-              <KonohaUp/>
-                <Box 
-                    display="flex" 
-                    justifyContent="center" 
-                    flexDirection="column"
-                    alignItems="center"
-                    >
-                    <Typography color="#474B5A" variant="h4">Нэвтрэх</Typography>
-                    <Box 
-                            display="flex" 
-                            flexDirection="column"
-                            gap='15px'
-                            width='400px'
-                            mt="30px"
-                        >
-                        <TextField value={email} onChange={e=>setEmail(e.target.value)}  label="ИМэйл" fullWidth/>
-                        <TextField label="Нууц үг" fullWidth/>
-                        <Button 
-                        onClick={()=>sentMessage()}  variant="contained" sx={{color: "#fff"  }} >Баталгаажуулах</Button>
-                    </Box>
-                </Box>
-                <Konoha/>
-          </Box>
-        </Container>
-    )
-}
+          Баталгаажуулах
+        </Button>
+      </Box>
+    </Grid>
+  );
+};
